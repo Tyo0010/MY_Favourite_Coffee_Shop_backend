fc15e88059b2101753087c0e763e5f222032903e
Tyo0010 <artem.pak24@gmail.com>
2025-05-14 10:18:02 +0800

    makes necessary changes

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
 from flask import Flask
-from app import create_app
+from app.views import blueprint
+from flask import abort, jsonify, request
 
-app = create_app()
+@blueprint.after_request
+def set_cors(response):
+    origin = request.headers.get('Origin')
+    response.headers['Access-Control-Allow-Origin'] = origin
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Content-Type'] = 'application/json'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
-if __name__ == "__main__":
+@blueprint.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Welcome to MY Favourite Coffee Shop API'})
+
+def create_app():  # flask app object
+    app = Flask(__name__)
+    app.config.from_object('config')  # Configuring from Python files
+    
+    return app
+
+# Creating the app
+app = create_app()  
+# Registering the blueprint
+app.register_blueprint(blueprint, url_prefix='/meritto')
+
+import os
+APP_ENV = os.environ.get('APP_ENV', 'development')
+
+if APP_ENV != 'production':
+    app.debug = True
+
+if __name__ == '__main__':  # Running the app
     app.run()
--- handler.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-
-# Create application for Lambda
-application = create_app()
-# This is the WSGI application object that Lambda will call
-app = application
